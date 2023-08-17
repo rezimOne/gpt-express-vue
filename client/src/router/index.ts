da0cfd9e851e38c1e26181d25b2d5f13329a03d5
rezimOne <mymac@MacBook-Pro-rezimOne.local>
2023-08-17 02:17:15 +0200

    clean

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -5,20 +5,12 @@ import PromptImager from '../pages/PromptImager'
 import ChatPage from '../pages/ChatPage';
 
 const router = createRouter({
-  /* history: to use the browser builtin support. How to manage routing history (router knows what last page in browser was) */
   history: createWebHistory(),
-  /* routes[]: to register different paths (urls) */
   routes: [
     { path: '/', component: HomePage },
     { path: '/about', component: AboutPage },
     { path: '/prompt-imager', component: PromptImager },
     { path: '/chat', component: ChatPage }
-    // route with params
-    //{ path: '/teams/:teamId', component: TeamMembers }
-    /*
-    our-domain.com/teams/anything
-    we can write 'anything' because anything after '.../teams/' will be interpreted as a value for ':teamId' dynamic segment
-    */
   ],
   linkActiveClass: 'active'
 });
